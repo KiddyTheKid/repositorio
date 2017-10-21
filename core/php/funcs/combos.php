bececd141054374f6d5aa8fcdb05645bf8905029
Pedro Acosta <pcost8300@me.com>
2017-10-21 16:47:09 -0500

    Nuevas funciones

--- core/php/funcs/combos.php
+++ core/php/funcs/combos.php
@@ -11,13 +11,21 @@ switch ($_POST['combo']){
     case 1:
         comboCarreras($con);
         break;
+    case 2:
+        comboDocumentosTipos($con);
+        break;
 }
 echo '</select>';
 
-
-
 function comboCarreras($con){
     $sql = "SELECT * FROM carreras";
+    $res = mysqli_query($con,$sql);
+    while($row = $res->fetch_array()){
+        echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+    }
+}
+function comboDocumentosTipos($con){
+    $sql = "SELECT * FROM tipos_documentos";
     $res = mysqli_query($con,$sql);
     while($row = $res->fetch_array()){
         echo '<option value="'.$row[0].'">'.$row[1].'</option>';
